f43979ba4507167cd5a985d88f0cf064c63a81e1
alanmarques144 <alanshalom12@gmail.com>
2024-08-30 21:09:53 -0300

    questao 1 respondida

--- src/questao01/RespostaQ1.java
+++ src/questao01/RespostaQ1.java
@@ -1,6 +1,20 @@
 package questao01;
+
+import java.util.Scanner;
+
 public class RespostaQ1 {
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.println("Digite um número:");
+        num = sc.nextInt();
+
+        VerificaFibonacci verifica = new VerificaFibonacci(num);
+
+        verifica.mostrarResultado();
+
+        sc.close();
     }
 }
--- src/questao01/VerificaFibonacci.java
+++ src/questao01/VerificaFibonacci.java
@@ -2,4 +2,32 @@ package questao01;
 
 public class VerificaFibonacci {
     
+    private int num;
+
+    public VerificaFibonacci(int num) {
+        this.num = num;
+    }
+
+
+    public boolean calcularFibonacci() {
+        int n1 = 0, n2= 1, prox = 0;
+
+        for (int i = 0; prox <= num; i++) {
+            if (prox == num) {
+                return true;
+            }
+            n1 = n2;
+            n2 = prox;
+            prox = n1 + n2;
+        }
+        return false;
+    }
+
+    public void mostrarResultado() {
+        if (calcularFibonacci()) {
+            System.out.println("O número informado pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println("O número não pertence à sequência de Fibonacci.");
+        }
+    }
 }
