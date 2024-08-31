fa693ed01c43c318876d0c7af10ee3df5214e731
alanmarques144 <alanshalom12@gmail.com>
2024-08-30 21:31:55 -0300

    questão 3 respondida

--- src/questao03/RespostaQ3.java
+++ src/questao03/RespostaQ3.java
@@ -1,5 +1,16 @@
 package questao03;
 
 public class RespostaQ3 {
-    
+    public static void main(String[] args) throws Exception {
+        
+        int indice = 12, soma = 0, k = 1;
+
+        while (k < indice) {
+            k=k+1;
+            soma = soma + k;
+        }
+
+        // O resultado é: 77
+        System.out.println("O resultado é: " + soma);
+    }
 }
