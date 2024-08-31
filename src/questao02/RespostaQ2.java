098ea0492a370b6ad6e427896cd0f5164223c288
alanmarques144 <alanshalom12@gmail.com>
2024-08-30 21:25:04 -0300

    questão 2 respondida

--- src/questao02/RespostaQ2.java
+++ src/questao02/RespostaQ2.java
@@ -1,5 +1,21 @@
 package questao02;
 
+import java.util.*;
+
 public class RespostaQ2 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.println("Digite a string: ");
+        String palavra = sc.nextLine();
+
+        VerificaCaracter verifica = new VerificaCaracter(palavra);
+
+        
+        verifica.mostrarResultado();
+
+        sc.close();
+    }
 }
--- src/questao02/VerificaCaracter.java
+++ src/questao02/VerificaCaracter.java
@@ -2,4 +2,32 @@ package questao02;
 
 public class VerificaCaracter {
     
+    
+    private String palavra;
+
+    public VerificaCaracter(String palavra) {
+        this.palavra = palavra;
+    }
+
+    public int contarLetras() {
+        
+        int count = 0;
+        for (int i = 0; i < palavra.length(); i++) {
+            char letra = palavra.charAt(i);
+            if(letra == 'a' || letra == 'A'){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public void mostrarResultado() {
+        int contador = contarLetras();
+        if(contador >= 1){
+                System.out.println("A letra 'a' aparece " + contador + " vezes!");
+        }else{        
+                System.out.println("A letra 'a' não aparece na string.");
+        }        
+    }
 }
